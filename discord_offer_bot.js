f148d8d12a614fe52c382a953c49c7e111a833ac
tONYz455 <filipzarebczan2425@gmail.com>
2026-03-29 23:41:07 +0200

    Update discord_offer_bot.js

--- discord_offer_bot.js
+++ discord_offer_bot.js
@@ -303,12 +303,6 @@ async function safeReply(interaction, payload) {
   return interaction.reply(payload);
 }
 
-  if (interaction.replied || interaction.deferred) {
-    return interaction.followUp(payload);
-  }
-  return interaction.reply(payload);
-}
-
 // 👇 DODAJ TO TU
 function getRandomPurchase() {
   const fakePurchases = [
